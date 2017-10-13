f9d18f3df9c40157b11e9d5bd87594045fc2a439
macroG0D <antidota2700@gmail.com>
2017-10-13 23:14:43 +0300

    yoga_lp
    yoga#1

---
+++ yoga/js/common.js
@@ -0,0 +1,33 @@
+var slideIndex=1;
+showSlides(slideIndex);
+
+function plusSlides(n){
+	showSlides(slideIndex += n);
+}
+
+/*function currentSlide(n){
+	showSlides(slideIndex = n);
+}*/
+
+function showSlides(n){
+	var i;
+	var slides = document.getElementsByClassName("sliders_item");
+
+	if(n > slides.length){
+		slideIndex = 1;
+	}
+	if(n < 1){
+		slideIndex = slides.length;
+	}
+	for(i=0; i<slides.length; i++){
+		slides[i].style.display = "none";
+	}
+
+	 slides[slideIndex-1].style.display = "block";
+}
+
+$(".toggle-mnu").click(function() {
+  $(this).toggleClass("on");
+  $(".hidden_menu").slideToggle();
+  return false;
+});
